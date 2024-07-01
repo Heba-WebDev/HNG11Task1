a7403a831a8473ad76558f20c0895412ab19dcac
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-01 18:05:08 +0300

    refactor

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import "dotenv/config";
+import geoip from 'geoip-lite';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,29 +10,23 @@ app.use(express.json());
 app.get(
   "/api/hello",
   async (req, res ) => {
-    const name = req.query.visitor_name || "visitor";
-    const ip = req.ip;
-    const loc = await getLocation;
-    const weatherResponse = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${loc.lat}&lon=${loc.lon}&units=metric&appid=${process.env.KEY}`
-    );
-    console.log("Weather res:", weatherResponse);
+   const visitorName = req.query.visitor_name || "Visitor";
+    const clientIp = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+    var geo = geoip.lookup(clientIp);
 
-    const weatherData = await weatherResponse.json();
-    console.log("Weather data:", weatherData);
-    console.log(weatherData?.main?.temp)
+    const weather = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${geo.ll[0]}&lon=${geo.ll[1]}&units=metric&appid=${process.env.KEY}`
+    ).then((data) => data.json());
+    console.log(weather)
     res.setHeader("Content-Type", "application/json");
     res.status(200).json({
-      client_ip: ip,
-      location: loc?.city,
-      greeting: `Hello, ${name}, the temperature is ${weatherData?.main?.temp} degrees Celcius in ${loc?.city}`,
+      client_ip: clientIp,
+      location: geo.city,
+      greeting: `Hello, ${visitorName}!, the temperature is ${weather.main.temp} degrees Celcius in ${geo.city}`,
     });
   }
 );
 
-const getLocation = fetch("http://ip-api.com/json/?fields=61439").then((data) =>
-  data.json()
-);
 
 app.listen(port,'0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
