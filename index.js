84c8dc5e6ff8ca990edcba70c4fb82d1e9cba083
Heba-Webdev23 <jtvonline@yahoo.com>
2024-07-01 17:09:47 +0300

    Fix: host settings

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import "dotenv/config";
 
 const app = express();
-const port = 5002 || process.env.PORT;
+const port = process.env.PORT || 3000;
 app.set("trust proxy", true);
 
 app.use(express.json());
@@ -29,6 +29,6 @@ const getLocation = fetch("http://ip-api.com/json/?fields=61439").then((data) =>
   data.json()
 );
 
-app.listen(port, () => {
+app.listen(port,'0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
